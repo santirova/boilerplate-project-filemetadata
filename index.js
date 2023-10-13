4d215588c8567232f3ea84d2f3ba43a2a99ce402
santilagos <sntlgs@gmail.com>
2023-10-13 19:29:57 -0300

    approved

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
 var app = express();
 
+
+const upload = multer({
+  dist:"/upload"
+  // storage: multer.diskStorage({
+  //   destination: './uploads',
+  //   filename: (req, file, cb) => {
+  //     const fileName = file.originalname + Date.now() + '.png';
+  //     cb(null, fileName);
+  //   },
+  // }),
+})
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,6 +22,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  res.send({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+      
+  })
+})
 
 
 
